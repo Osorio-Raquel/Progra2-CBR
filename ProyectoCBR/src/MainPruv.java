60783354361f7498ce6b3fb31b59280b8624ad4c
rafaCordova0623 <rafaela.cordova@ucb.edu.bo>
2025-05-10 13:40:37 -0400

    Update MainPruv.java

--- ProyectoCBR/src/MainPruv.java
+++ ProyectoCBR/src/MainPruv.java
@@ -4,6 +4,8 @@ public class MainPruv {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+
+		//HOLA ÉSTA ES UNA PRUEBA PARA LOS GITMERGE
 	}
 
 }
