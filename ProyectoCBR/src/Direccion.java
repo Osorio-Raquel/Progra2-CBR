44906e22e7d1235a6cb3dbeed6b13272a4adef2b
diegoruiz111 <diegoandresruizzz1@gmail.com>
2025-05-11 00:05:09 -0400

    Update Direccion.java

--- ProyectoCBR/src/Direccion.java
+++ ProyectoCBR/src/Direccion.java
@@ -1,1 +1,45 @@
+import java.time.LocalDate;
+
+public class Direccion {
+	private String zona;
+	private String departamento; 
+	private String calle;
+	private String nro;
+	public String getZona() {
+		return zona;
+	}
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+	public String getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public Direccion(String zona, String departamento, String calle, String nro) {
+		super();
+		this.zona = zona;
+		this.departamento = departamento;
+		this.calle = calle;
+		this.nro = nro;
+	}
+	public String getNro() {
+		return nro;
+	}
+	public void setNro(String nro) {
+		this.nro = nro;
+	}
+	
+
+	
+
+
+}
 
