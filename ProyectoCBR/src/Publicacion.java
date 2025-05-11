729b144dc661cc1b06ed024818195dd7f975c55e
AndresBaldivieso <andres.baldivieso@ucb.edu.bo>
2025-05-10 23:40:53 -0400

    Create Publicacion.java

---
+++ ProyectoCBR/src/Publicacion.java
@@ -0,0 +1,80 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Publicacion {
+	private LocalDate fechaPublicacion;
+	private Artista autor;
+	private String imagen;
+	private String descripcion;
+	private boolean estado;
+	private ArrayList<Comentario> comentarios;
+	
+	public Publicacion(LocalDate fechaPublicacion, Artista autor, String imagen, String descripcion, boolean estado,
+			ArrayList<Comentario> comentarios) {
+		this.fechaPublicacion = fechaPublicacion;
+		this.autor = autor;
+		this.imagen = imagen;
+		this.descripcion = descripcion;
+		this.estado = estado;
+		this.comentarios = new ArrayList<>();
+	}
+
+	public LocalDate getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+
+	public void setFechaPublicacion(LocalDate fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+
+	public Artista getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Artista autor) {
+		this.autor = autor;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public ArrayList<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(ArrayList<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+	
+	@Override
+	public String toString() {
+		return "Publicacion [fechaPublicacion=" + fechaPublicacion + ", autor=" + autor + ", imagen=" + imagen
+				+ ", descripcion=" + descripcion + ", estado=" + estado + ", comentarios=" + comentarios + "]";
+	}
+
+	public void agregarComentario(Comentario c) {
+        comentarios.add(c);
+    }
+	
+}
